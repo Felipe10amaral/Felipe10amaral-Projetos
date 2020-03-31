22905d8c978d181d2d560ac0aadfaff91ac5d070
Felipe Amaral <felipe.sf2011@gmail.com>
2020-03-31 18:50:45 -0300

    frontend

---
+++ beTheHero/frontend/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './global.css'
+import Routes from './routes'
+
+
+// JSX (JavaScript XML) codigo de html no javascript (HTML integrado no javascript)
+//useState retorna um valor com 2 posições [valor fda variavel, segunda posição e uma função que possibilita alterar o valor ]
+
+
+function App() {
+  return (
+    <Routes />
+  );
+}
+
+export default App;
