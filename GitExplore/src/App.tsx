345df9ea8524ca74fa10d6fcd7205719a263f896
Felipe Amaral <felipe.sf2011@gmail.com>
2021-01-17 15:56:00 -0300

    GoStack - GitHub Explore

---
+++ GitExplore/src/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import {BrowserRouter} from 'react-router-dom';
+import Routes from './routes';
+import Global from './styles/global';
+
+const App:  React.FC = () => (
+  <>
+    <BrowserRouter>
+      <Routes />
+    </BrowserRouter>
+    <Global />
+  </>
+)
+
+export default App;
